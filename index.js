6bd6f256b1e75fa6730ff245d7f20c1c87a5234d
Aswun <aswinlutfian32@gmail.com>
2025-10-21 10:06:17 +0700

    menambahkan connection ke databbase

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`)
 });
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Bil1o8775',
+  database: 'biodata',
+  port: 3307
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connection successful!');
+});
